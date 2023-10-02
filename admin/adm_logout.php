3a8cbde411dad5ee5d5f910babd54d55ea7ed452
Prihadi28 <126578494+Prihadi28@users.noreply.github.com>
2023-10-02 14:05:13 +0700

    Delete admin directory

--- admin/adm_logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin']);
-echo "<script language='javascript'>alert('Terima kasih, Anda Berhasil Logout'); document.location='adm_login.php';</script>";
-?>
